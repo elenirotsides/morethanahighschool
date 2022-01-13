2e94ed44ef214ba2cbd95ea8b2614516d9d35230
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2022-01-12 22:07:11 -0500

    content on home and vote pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import img from './img/img1.png';
+// import img from './img/img1.png';
+import img2 from './img/img2.png';
+import logo from './img/logo.png';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
-
+import TwitterIcon from '@material-ui/icons/Twitter';
 import Home from './components/Home';
 import About from './components/About';
 import Vote from './components/Vote';
@@ -12,22 +14,29 @@ import Contact from './components/Contact';
 
 const App = () => {
     return (
-        <div>
+        <div style={{ backgroundColor: '#b1b1b1' }}>
             <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
-                <Container fluid>
-                    <Navbar.Brand href='/' style={{ color: 'white', fontWeight: 'bold' }}>
-                        Friends of the New Hoboken High School
+                <Container fluid style={{ textAlign: 'center' }}>
+                    <Navbar.Brand href='/'>
+                        <img
+                            src={logo}
+                            style={{ width: '70px', marginRight: '30px' }}
+                            alt='The logo of "Friends of the New Hoboken High School" organization'
+                        />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                     <Navbar.Collapse id='responsive-navbar-nav'>
                         <Nav className='me-auto'>
-                            <Nav.Link href='about' style={{ color: 'white' }}>
+                            <Nav.Link className='navLink' href='about' style={{ color: 'white', fontSize: '20px' }}>
                                 About
                             </Nav.Link>
-                            <Nav.Link href='vote' style={{ color: 'white' }}>
+                            <Nav.Link className='navLink' href='vote' style={{ color: 'white', fontSize: '20px' }}>
                                 Vote
                             </Nav.Link>
-                            <Nav.Link href='contact' style={{ color: 'white' }}>
+                            <Nav.Link className='navLink' href='op-ed' style={{ color: 'white', fontSize: '20px' }}>
+                                Op-Eds
+                            </Nav.Link>
+                            <Nav.Link className='navLink' href='contact' style={{ color: 'white', fontSize: '20px' }}>
                                 Contact
                             </Nav.Link>
                         </Nav>
@@ -36,27 +45,40 @@ const App = () => {
                                 <FacebookIcon
                                     style={{ fontSize: '40px', cursor: 'pointer', color: '#d60001' }}
                                     onClick={() => window.open('https://www.facebook.com/Morethanahighschool', '_blank')}
+                                    aria-label="Opens a new window to Friends of the New Hoboken High School's Facebook Page"
                                 />
                             </Nav.Link>
                             <Nav.Link>
                                 <InstagramIcon
                                     style={{ fontSize: '40px', cursor: 'pointer', color: '#d60001' }}
                                     onClick={() => window.open('https://www.instagram.com/morethanahighschool/', '_blank')}
+                                    aria-label="Opens a new window to Friends of the New Hoboken High School's Instagram Page"
+                                />
+                            </Nav.Link>
+                            <Nav.Link>
+                                <TwitterIcon
+                                    style={{ fontSize: '40px', cursor: 'pointer', color: '#d60001' }}
+                                    onClick={() => window.open('https://twitter.com/newhobokenhs/', '_blank')}
+                                    aria-label="Opens a new window to Friends of the New Hoboken High School's Twitter Page"
                                 />
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <Container fluid style={{ backgroundColor: '#d60001' }}>
-                <img src={img} style={{ height: '400px' }} alt="Graphic that urges voters to vote 'Yes' on Jan. 25"></img>
+            <Container fluid style={{ backgroundColor: '#d60001', textAlign: 'center' }}>
+                <Row>
+                    <Col xs={12}>
+                        <img src={img2} alt="Graphic that urges voters to vote 'Yes' on Jan. 25" style={{ width: '75%' }} />
+                    </Col>
+                </Row>
             </Container>
             {/* <Carousel variant='dark'>
-                <Carousel.Item style={{ height: '400px' }}>
-                    <img className='d-block w-100' src={img} alt='First slide' style={{ height: '400px' }} />
+                <Carousel.Item style={{ height: '400px', backgroundColor: '#d60001' }}>
+                    <img src={img2} alt='First slide' style={{ height: '400px' }} />
                 </Carousel.Item>
-                <Carousel.Item style={{ height: '400px' }}>
-                    <img className='d-block w-100' src={logo} alt='Second slide' style={{ height: '400px' }} />
+                <Carousel.Item style={{ height: '400px', backgroundColor: '#cb0a32' }}>
+                    <img src={logo} alt='Second slide' style={{ height: '400px' }} />
                 </Carousel.Item>
             </Carousel> */}
             <BrowserRouter>
@@ -67,6 +89,37 @@ const App = () => {
                     <Route path='contact' element={<Contact />} />
                 </Routes>
             </BrowserRouter>
+            <Navbar bg='dark' variant='dark'>
+                <Container fluid>
+                    <Navbar.Text className='navText m-2'>
+                        <span style={{ fontSize: '20px' }}>Paid for by the Friends of the New Hoboken High School</span> <br />
+                        <span style={{ fontSize: '15px' }}>600 Jackson Street, Hoboken, NJ 07030</span>
+                    </Navbar.Text>
+                    <Nav>
+                        <Nav.Link>
+                            <FacebookIcon
+                                style={{ fontSize: '20px', cursor: 'pointer', color: '#d60001' }}
+                                onClick={() => window.open('https://www.facebook.com/Morethanahighschool', '_blank')}
+                                aria-label="Opens a new window to Friends of the New Hoboken High School's Facebook Page"
+                            />
+                        </Nav.Link>
+                        <Nav.Link>
+                            <InstagramIcon
+                                style={{ fontSize: '20px', cursor: 'pointer', color: '#d60001' }}
+                                onClick={() => window.open('https://www.instagram.com/morethanahighschool/', '_blank')}
+                                aria-label="Opens a new window to Friends of the New Hoboken High School's Instagram Page"
+                            />
+                        </Nav.Link>
+                        <Nav.Link>
+                            <TwitterIcon
+                                style={{ fontSize: '20px', cursor: 'pointer', color: '#d60001' }}
+                                onClick={() => window.open('https://twitter.com/newhobokenhs/', '_blank')}
+                                aria-label="Opens a new window to Friends of the New Hoboken High School's Twitter Page"
+                            />
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,77 @@
+import { Container, Row, CardGroup, Card, Button } from 'react-bootstrap';
 import '../App.css';
+import Facilities from '../img/Facilities.png';
+import Dangers from '../img/Dangers.png';
+import Lowdown from '../img/Lowdown.png';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home page content will go here</h1>
-            <p className='hometext'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolorem quos molestias libero dolore. Placeat saepe possimus
-                deserunt alias dolor, itaque dolorum repudiandae. Voluptatibus ducimus, modi vitae quia ipsa quas? Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Ipsam, mollitia illo? Adipisci nulla illum dolores quisquam beatae animi, cum fuga id voluptatum, ipsam,
-                at excepturi amet corporis enim voluptas molestiae? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea qui ipsa sit quaerat,
-                incidunt nisi amet accusamus eaque nihil consequatur voluptates iure voluptate, sapiente quisquam dolorum, libero possimus ut? Id?
-            </p>
-        </div>
+        <Container>
+            <Row>
+                <h1 style={{ textAlign: 'center', paddingTop: '20px' }}>—Together, we can build Hoboken's future—</h1>
+            </Row>
+            <Row style={{ paddingTop: '20px' }}>
+                <CardGroup>
+                    <Card className='m-3' style={{ textAlign: 'center' }}>
+                        <Card.Img variant='top' src={Facilities} />
+                        <Card.Body>
+                            <Card.Title>Facilities: Why the Proposed Plan Is Necessary Now</Card.Title>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button
+                                variant='danger'
+                                onClick={() =>
+                                    window.open(
+                                        'https://docs.google.com/document/d/1vumTHAgDrjjAXTqi7obbFMIAiSHKqfmlmk-Fn5x_1E8/edit?usp=sharing',
+                                        '_blank'
+                                    )
+                                }
+                            >
+                                Click to Read
+                            </Button>
+                        </Card.Footer>
+                    </Card>
+                    <Card className='m-3' style={{ textAlign: 'center' }}>
+                        <Card.Img variant='top' src={Dangers} />
+                        <Card.Body>
+                            <Card.Title>The Dangers of “No for Now”: What a “no” vote really means</Card.Title>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button
+                                variant='danger'
+                                onClick={() =>
+                                    window.open(
+                                        'https://docs.google.com/document/d/18LIZKapeUQVCTrFDjXtO-QLUNgT4enBma9vJKrY-9MM/edit?usp=sharing',
+                                        '_blank'
+                                    )
+                                }
+                            >
+                                Click to Read
+                            </Button>
+                        </Card.Footer>
+                    </Card>
+                    <Card className='m-3' style={{ textAlign: 'center' }}>
+                        <Card.Img variant='top' src={Lowdown} />
+                        <Card.Body>
+                            <Card.Title>Calculate Your Tax Increase</Card.Title>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Button
+                                variant='danger'
+                                onClick={() =>
+                                    window.open(
+                                        'https://docs.google.com/document/d/1_57FxjiLKfAa2cazpSaui3IjNECCVJXaM3EI91XtW_k/edit?usp=sharing',
+                                        '_blank'
+                                    )
+                                }
+                            >
+                                Click to Read
+                            </Button>
+                        </Card.Footer>
+                    </Card>
+                </CardGroup>
+            </Row>
+        </Container>
     );
 };
 
