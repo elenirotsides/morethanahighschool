10526557ccd8c45f1c7b104859a2fa79075bb915
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2022-01-16 21:59:23 -0500

    omg

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }) {
     return (
